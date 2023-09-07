386c7995d49bee5f32708ddd09bbaf3de078a0fb
LalitaSoso <127132789+LalitaSoso@users.noreply.github.com>
2023-09-06 20:15:03 -0700

    changed cities in the drop down, added 2nd city to the landing page along with building function for cities in the drop down

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,45 @@
 function updateTime() {
+	// The Bronx
 	let theBronxElement = document.querySelector("#the-bronx");
-	let theBronxDateElement = theBronxElement.querySelector(".date");
-	let theBronxTimeElement = theBronxElement.querySelector(".time");
-	let theBronxTime = moment().tz("America/New_York");
+	if (theBronxElement) {
+		let theBronxDateElement = theBronxElement.querySelector(".date");
+		let theBronxTimeElement = theBronxElement.querySelector(".time");
+		let theBronxTime = moment().tz("America/New_York");
 
-	theBronxDateElement.innerHTML = theBronxTime.format("ddd MMMM Do");
-	theBronxTimeElement.innerHTML = theBronxTime.format("h:mm:ss [<small>]A[</small>]");
+		theBronxDateElement.innerHTML = theBronxTime.format("ddd MMMM Do");
+		theBronxTimeElement.innerHTML = theBronxTime.format("h:mm:ss [<small>]A[</small>]");
+	}
+
+	// Girona
+	let gironaElement = document.querySelector("#girona");
+	if (gironaElement) {
+		let gironaDateElement = gironaElement.querySelector(".date");
+		let gironaTimeElement = gironaElement.querySelector(".time");
+		let gironaTime = moment().tz("Europe/Madrid");
+
+		gironaDateElement.innerHTML = gironaTime.format("ddd MMMM Do");
+		gironaTimeElement.innerHTML = gironaTime.format("h:mm:ss [<small>]A[</small>]");
+	}
+}
+
+function updateCity(event) {
+	let cityTimeZone = event.target.value;
+	let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+	let cityTime = moment().tz(cityTimeZone);
+	let citiesElement = document.querySelector("#cities");
+	citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+  </div>
+  `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
